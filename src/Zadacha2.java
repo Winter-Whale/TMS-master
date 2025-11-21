f3a048bee8598ccbba5c1eb0417e6bf464484f8f
Виталий <vitalij.melnikov.2012@gmail.com>
2025-11-21 15:50:12 +0300

    задачи по второй лекции

---
+++ src/Zadacha2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Zadacha2 {
+
+    public static void main(String[] args){
+
+        System.out.println("Введите температуру");
+
+        Scanner temp = new Scanner(System.in);
+        int t = temp.nextInt();
+        if (t>-5){
+            System.out.println("Warm");
+        } if(t<-20) {
+            System.out.println("Cold");
+        } if((-5>=t&t>-20)){
+            System.out.println("Normal");
+        }
+
+    }
+}
---
+++ src/Zadacha3.java
@@ -0,0 +1,9 @@
+public class Zadacha3 {
+
+    public static void main(String[] args){
+        int b =2;
+      for(int i =10; i<=20; i++){
+          System.out.println((int)Math.pow(i,b));
+      }
+    }
+}

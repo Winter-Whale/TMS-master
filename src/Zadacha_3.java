702cebf05859fd041075aa7b812cf1fac8d70269
Виталий <vitalij.melnikov.2012@gmail.com>
2025-11-21 15:39:43 +0300

    Zadachi po 1 lekcii

---
+++ src/Zadacha_3.java
@@ -0,0 +1,12 @@
+public class Zadacha_3 {
+
+    public static void main(String[] args){
+
+        int n = 126;
+        int a = n/100;
+        int b = (n/10)%10;
+        int c = n%10;
+        System.out.println(a+b+c);
+
+    }
+}

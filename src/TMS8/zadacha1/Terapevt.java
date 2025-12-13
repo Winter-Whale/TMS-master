65033ee161418b6df9b5c61294fac88f9034c8db
Виталий <vitalij.melnikov.2012@gmail.com>
2025-12-14 00:22:15 +0300

    TMS8

---
+++ src/TMS8/zadacha1/Terapevt.java
@@ -0,0 +1,10 @@
+package TMS8.zadacha1;
+
+class Terapevt extends Doctor {
+    String name = "Терапевт";
+
+    @Override
+    String treatment() {
+        return "Проведет осмотр, выпишет лекарства, запишет на прием";
+    }
+}
---
+++ src/TMS8/zadacha2/newApple.java
@@ -0,0 +1,9 @@
+package TMS8.zadacha2;
+
+public class newApple {
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        apple.setColorInternal("Yellow");
+        System.out.println(apple.getColor());
+    }
+}

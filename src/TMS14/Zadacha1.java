3c94b18aae1fe43f13eee7b81fa3afc68146ed36
Виталий <vitalij.melnikov.2012@gmail.com>
2026-01-16 14:44:22 +0300

    TMS13

--- src/TMS14/Zadacha1.java
+++ src/TMS14/Zadacha1.java
@@ -5,7 +5,7 @@ import java.nio.file.*;
 import java.util.*;
 
 public class Zadacha1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String inputFile = "E:/ДЗ ТМС/Romeo.txt";
         String outputFile = "longword.txt";
         try {
@@ -18,9 +18,9 @@ public class Zadacha1 {
                 }
             }
             Files.writeString(Path.of(outputFile), longword);
-            System.out.println("Самое длинное слово "+ longword);
-            System.out.println("Результат в файле "+ outputFile);
-        }  catch (IOException e){
+            System.out.println("Самое длинное слово " + longword);
+            System.out.println("Результат в файле " + outputFile);
+        } catch (IOException e) {
             System.out.println("Ошибка " + e.getMessage());
         }
     }
